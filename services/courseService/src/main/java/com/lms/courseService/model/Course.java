f3e47a19b56a13c96a03d1ac279058f2c71162be
hristo-darlyanov <83130557+hristo-darlyanov@users.noreply.github.com>
2025-10-04 17:06:29 +0300

    LMS-31: Refactoring of code

--- services/courseService/src/main/java/com/lms/courseService/model/Course.java
+++ services/courseService/src/main/java/com/lms/courseService/model/Course.java
@@ -2,13 +2,20 @@ package com.lms.courseService.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "courses")
 public class Course {
+
     @Id
     private String id;
 
@@ -25,59 +32,4 @@ public class Course {
 
     @NotNull
     private CourseStatus status = CourseStatus.CREATED;
-
-    public Course() { }
-
-    public Course(String id, String name, String description, List<String> students, Integer duration, CourseStatus status) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.students = students;
-        this.duration = duration;
-        this.status = status;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public List<String> getStudents() {
-        return students;
-    }
-
-    public void setStudents(List<String> students) {
-        this.students = students;
-    }
-
-    public Integer getDuration() {
-        return duration;
-    }
-
-    public void setDuration(Integer duration) {
-        this.duration = duration;
-    }
-
-    public CourseStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(CourseStatus status) {
-        this.status = status;
-    }
 }
