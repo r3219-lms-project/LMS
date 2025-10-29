2ce56b862c876b09e0babb458df20a25f7a0359f
undndnwnkk <bykov.lev74@gmail.com>
2025-10-29 14:42:11 +0300

    LMS-46: added jwt token provider with parsing access tokens

--- services/common-security/src/main/java/ru/lms_project/common/security/ParsedToken.java
+++ services/common-security/src/main/java/ru/lms_project/common/security/ParsedToken.java
@@ -1,11 +1,13 @@
 package ru.lms_project.common.security;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 import java.util.UUID;
 
 @Data
+@AllArgsConstructor
 public class ParsedToken {
     private UUID userId;
     private List<String> roles;
