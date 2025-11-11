0a42f8537089b1a4f2c2195aab4386d47038091c
Rianexi <rusushechka2@gmail.com>
2025-11-11 22:47:54 +0300

    LMS-56:refactoring

--- services/progressService/src/test/java/com/lms/progressservice/ProgressServiceApplicationTests.java
+++ services/progressService/src/test/java/com/lms/progressservice/ProgressServiceApplicationTests.java
@@ -2,14 +2,8 @@ package com.lms.progressService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-    "spring.datasource.url=jdbc:h2:mem:testdb",
-    "spring.datasource.driver-class-name=org.h2.Driver",
-    "spring.jpa.hibernate.ddl-auto=create-drop"
-})
 class ProgressServiceApplicationTests {
 
     @Test
